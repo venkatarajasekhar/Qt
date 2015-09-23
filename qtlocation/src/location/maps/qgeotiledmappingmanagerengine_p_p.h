74df731e6f20df628ff18f26121d64db3d6f63f5
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-09-23 20:07:07 +0530

    qgeotiledmappingmanagerengine_p_p.h

--- qtlocation/src/location/maps/qgeotiledmappingmanagerengine_p_p.h
+++ qtlocation/src/location/maps/qgeotiledmappingmanagerengine_p_p.h
@@ -36,8 +36,8 @@
 #define QGEOTILEDMAPPINGMANAGER_P_H
 
 //
-//  W A R N I N G
-//  -------------
+//  
+// 
 //
 // This file is not part of the Qt API.  It exists purely as an
 // implementation detail.  This header file may change from version to
