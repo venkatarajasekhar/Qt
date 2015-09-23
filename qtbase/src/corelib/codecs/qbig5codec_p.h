05dd468709057903617347ea920a5c8fa2b68f37
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-09-23 20:25:32 +0530

    qbig5codec_p.h

--- qtbase/src/corelib/codecs/qbig5codec_p.h
+++ qtbase/src/corelib/codecs/qbig5codec_p.h
@@ -39,8 +39,8 @@
 #define QBIG5CODEC_P_H
 
 //
-//  W A R N I N G
-//  -------------
+// 
+//  
 //
 // This file is not part of the Qt API.  It exists for the convenience
 // of other Qt classes.  This header file may change from version to
