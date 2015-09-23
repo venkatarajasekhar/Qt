69f4a09aeb487508f1cef861a53583b5253c8fe2
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-09-23 19:48:36 +0530

    qgeotilespec_p_p.h

--- qtlocation/src/location/maps/qgeotilespec_p_p.h
+++ qtlocation/src/location/maps/qgeotilespec_p_p.h
@@ -34,8 +34,8 @@
 #define QGEOTILESPEC_P_H
 
 //
-//  W A R N I N G
-//  -------------
+//  
+// 
 //
 // This file is not part of the Qt API.  It exists purely as an
 // implementation detail.  This header file may change from version to
